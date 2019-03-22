54c5ccb6104ff6afa4680955b25274fcc9c7a03b
gabewei <ggdoubleu@gmail.com>
2019-03-22 12:26:12 -0600

    March 22nd

---
+++ Creative/app/src/main/java/org/westada/creative/MainActivity.java
@@ -0,0 +1,41 @@
+package org.westada.creative;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+
+    ListView myListView;
+    String[] items;
+    String[] prices;
+    String[] descriptions;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Resources res = getResources();
+        myListView = findViewById(R.id.myListView);
+        items = res.getStringArray(R.array.items);
+        prices = res.getStringArray(R.array.prices);
+        descriptions = res.getStringArray(R.array.descriptions);
+
+        ItemAdapter itemAdapter = new ItemAdapter(this, items, prices, descriptions);
+        myListView.setAdapter(itemAdapter);
+
+        myListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent showDetailActivity = new Intent(getApplicationContext(), DetailActivity.class);
+                showDetailActivity.putExtra("org.westada.ITEM_INDEX", position);
+                startActivity(showDetailActivity);
+            }
+        });
+    }
+}
